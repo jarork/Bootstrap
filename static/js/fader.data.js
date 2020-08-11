ed35e3ea315530382a23fd2ccd1d80ee10e14d19
jarork <jarork@qq.com>
2020-08-11 22:19:22 +0800

    fader and menu features

---
+++ static/js/fader.data.js
@@ -0,0 +1,14 @@
+var faderData = [
+    {
+        "img_url":"card1.jpg",
+        "img_info":"爬虫微课5小时 python学习路线!"
+    },
+    {
+        "img_url":"card2.jpeg",
+        "img_info":"干货|如何优雅的在手机上进行Python编程"
+    },
+    {
+        "img_url":"card3.jpg",
+        "img_info":"python-win10下使用定时任务执行程序"
+    }
+]
